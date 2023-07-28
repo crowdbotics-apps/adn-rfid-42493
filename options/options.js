f6235c4a151436535487a6f49ad0b36907b3a20a
Crowdbotics <team@crowdbotics.com>
2023-07-28 21:07:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "adn-rfid-42493",
+  url: "https://adn-rfid-42493.botics.co",
+  api: "https://adn-rfid-42493.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
